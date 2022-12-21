2cb702e9c71541f39adb93fe1cb7571b0a2e6502
Noor <98385039+noorotk@users.noreply.github.com>
2022-12-21 19:45:47 +0300

    **

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,7 +1,18 @@
-import React from "react";
-
+import React, { Fragment } from "react";
+import { Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 const QuoteDetail = () => {
-  return <div>QuoteDetail</div>;
+  const params = useParams();
+  return (
+    <Fragment>
+      <div>QuoteDetail</div>
+
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
